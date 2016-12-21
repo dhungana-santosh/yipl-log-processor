3861e2d01c6db1a2882c95d4392b592c57aa98da
= <=>
2016-12-21 19:51:48 +0545

    deleted some files

--- process.php
+++ process.php
@@ -1,53 +1,59 @@
-<html>
-<head>
-    <title>Summarize log</title>
-</head>
-<body>
-<?php
-$file = fopen('example.log', 'r') or die ('File opening failed');
-$line = fgets($file);
+	<?php
+	$handle=fopen('example.log', 'r') or die('File Could not be opened !!');
+	$line=fgets($handle);
 
-while (!feof($file)) {
+	while(! feof($handle))
+	{		
+			$requestsCount = 1;
+			$warningCounter=0;
+			$errorCounter=0;
+			$prevParts=explode(' ', $line);
+			$prevDate=explode('-', $prevParts[0]);
+			$prevDay=substr($prevParts[0],9,2);
+			$message=substr($prevParts[7], 0,10);
+			if(checkWarning($message)) $warningCounter++;
+			if(checkError($message)) $errorCounter++;
 
-    $requestsCount = 1;
-    $warning_count = 0;
-    $error_count = 0;
-    $parts = explode(' ', $line);
-    $date = substr($parts[0], 0, 10);
-    $message = substr($parts[7], 0, 10);
-    if (messagewarning($message)) $warning_count++;
-    if (messageerror($message)) $error_count++;
-    l:
-    $line1 = fgets($file);
-    $parts1 = explode(' ', $line1);
-    $date1 = substr($parts1[0], 0, 10);
-    if ($date == $date1) {
-        $requestsCount = 0;
-        $message = substr($parts1[7], 0, 10);
-        if (messagewarning($message)) $warning_count++;
-        if (messageerror($message)) $error_count++;
+			sameDay:
 
-        Goto l;
-    }
+			$nextLine=fgets($handle);
+			$nextPart=explode(' ', $nextLine);
+			$nextDate=explode('-', $nextPart[0]);
+			
+			$nextDay=substr($nextPart[0],9,2);
 
+			if($prevDay==$nextDay){
+				$requestsCount = 0;
+				$message=substr($nextPart[7], 0,10);
+				if(checkWarning($message)) $warningCounter++;
+				if(checkError($message)) $errorCounter++;
+				Goto sameDay;
+			}
 
-    echo($date . " warning:" . $warning_count . " error:" . $error_count . "<br>");
+	 	echo($prevParts[0] . " warning:" . $warningCounter . " error:" . $errorCounter . "<br><br>");
 
-    $line = $line1;
+		$line=$nextLine;
+		
 
 
-}
-fclose($file);
-function messagewarning($message)
-{
-    return substr_count($message, "warning") > 0;
-}
+	}
 
-function messageerror($message)
-{
-    return substr_count($message, "error") > 0;
-}
+	fclose($handle);
 
-?>
-</body>
-</html>
+	function checkWarning($msg){
+		if(preg_match('/warning/', $msg))
+			return true;
+	}
+
+	function checkError($msg){
+		if(preg_match('/error/', $msg))
+			return true;
+	}
+
+
+
+
+
+
+
+	?>
